b8b3af7643f074d93b5092b62cb5101134821874
Ezebube Kenechukwu David <71064387+urfavprogrammer@users.noreply.github.com>
2025-07-10 15:57:58 +0000

    My first commit

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,40 @@
 // Write your code here
+
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies
+  }
+  
+  get availability () {
+    return this.getAvailability();
+  }
+
+  getAvailability () {
+    if (this.numCopies === 0) {
+      return "Out of Stock";
+    } else if (this.numCopies < 10) {
+      return "Low Stock";
+    } 
+    return "In Stock"
+  }
+
+  sell(numCopiesSold = 1) {
+     this.numCopies -= numCopiesSold 
+  }
+
+  restock(numCopiesRestocked = 5) {
+     this.numCopies += numCopiesRestocked
+  }
+}
+
+ const HungerGames = new Book ("Hunger Games", "Suzanne Collins", 123919, 5);
+ console.log(HungerGames.getAvailability());
+
+ HungerGames.restock(12);
+ console.log(HungerGames.getAvailability());
+
+ HungerGames.sell(17);
+ console.log(HungerGames.getAvailability());
