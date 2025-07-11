fcc0d93e6f62e1aea9278b1bc402d84edb35f36e
Ezebube Kenechukwu David <71064387+urfavprogrammer@users.noreply.github.com>
2025-07-11 14:34:50 +0000

    second commikt

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,19 @@
 // Write your code here
+class movie {
+  constructor(title, director, genre, releaseYear, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating
+  }
+   getOverview() {
+    return  `${this.title}, a ${this.genre}, film directed by ${this.director}, was released
+      in ${this.releaseYear}, it received a rating of ${this.rating} `
+  }
+}
+
+const GameofThrones = new movie ("Game of Thrones",
+  "Frank Onuoha", "Fiction", 2016, "⭐️⭐️⭐️⭐️⭐️"
+)
+console.log(GameofThrones.getOverview())
